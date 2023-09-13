753e04dac44d5dd73b0ed19bdba3d63ecc5f7b35
Frankie Dick <fhd96@hotmail.co.uk>
2023-09-13 14:38:15 +0100

    changed file stircture

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-    <title>Template</title>
+$db = new PDO('mysql:host=db; dbname=collector_app', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    <meta name="description" content="Template HTML file">
-    <meta name="author" content="iO Academy">
+$query = $db->prepare(
+    "SELECT `teams`.`name`, `shirts`.`season`, `shirts`.`type`, `brands`.`name` as 'brand_name', `leagues`.`name` as 'league_name', `countries`.`name` as 'country' FROM `shirts`
+	INNER JOIN `teams`
+	ON `shirts`.`team_id` = `teams`.`id`
+	INNER JOIN `brands`
+	ON `shirts`.`brand_id` = `brands`.`id`
+	INNER JOIN `leagues`
+	ON `teams`.`league_id` = `leagues`.`id`
+	INNER JOIN `countries`
+	ON `leagues`.`country_id` = `countries`.`id`;");
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+$query->execute();
 
-    <link rel="icon" href="images/favicon.png" sizes="192x192">
-    <link rel="shortcut icon" href="images/favicon.png">
-    <link rel="apple-touch-icon" href="images/favicon.png">
+$shirts = $query->fetchALL();
 
-    <script defer src="js/index.js"></script>
-</head>
+foreach($shirts as $shirt) {
+    echo "<div class='item-container'>";
+    echo "<h2>{$shirt['name']}</h2>";
+    echo "<p>" . $shirt['season'] . " " . $shirt['type'] . " " . "kit</p>";
+    echo "<div>";
+    echo "<ul>";
+    echo "<li>{$shirt['league_name']}";
+    echo "<li>{$shirt['country']}";
+    echo "<li>Made by {$shirt['brand_name']}";
+    echo "</div>";
+    echo "</div>";
+}
 
-<body>
 
-<h1>Website Template</h1>
-
-</body>
-</html>

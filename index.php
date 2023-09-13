5b5e8c29628476ed6bf9cdae9c20cffdaba7ef6f
Frankie Dick <fhd96@hotmail.co.uk>
2023-09-13 15:19:31 +0100

    added simple navbar and some basic CSS

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Template</title>
+
+    <meta name="description" content="Template HTML file">
+    <meta name="author" content="iO Academy">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+
+    <script defer src="js/index.js"></script>
+</head>
+
+<body>
+    <nav class = 'navbar'>
+        <h2>Footie Shirts: My Collection</h2>
+        <div class = 'nav-links'>
+            <p>Home</p>
+            <p>Add</p>
+            <p>Edit</p>
+        </div>  
+        <form>
+            <input type="text" placeholder="search" name="search">
+            <button type=submit>Search</button>  
+    </nav>
+
+</body>
+</html>
+
 <?php
 
 $db = new PDO('mysql:host=db; dbname=collector_app', 'root', 'password');

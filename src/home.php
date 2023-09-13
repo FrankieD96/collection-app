cd63ee7aeaef2703a9b86fb512f8e1186177cfe0
Frankie Dick <fhd96@hotmail.co.uk>
2023-09-13 13:29:20 +0100

    added more to query and displayed extra stats for each shirt

--- src/home.php
+++ src/home.php
@@ -4,9 +4,15 @@ $db = new PDO('mysql:host=db; dbname=collector_app', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $query = $db->prepare(
-    "SELECT `teams`.`name`, `shirts`.`season`, `shirts`.`type` FROM `shirts`
-    INNER JOIN `teams`
-    ON `shirts`.`team_id` = `teams`.`id`;");
+    "SELECT `teams`.`name`, `shirts`.`season`, `shirts`.`type`, `brands`.`name` as 'brand_name', `leagues`.`name` as 'league_name', `countries`.`name` as 'country' FROM `shirts`
+	INNER JOIN `teams`
+	ON `shirts`.`team_id` = `teams`.`id`
+	INNER JOIN `brands`
+	ON `shirts`.`brand_id` = `brands`.`id`
+	INNER JOIN `leagues`
+	ON `teams`.`league_id` = `leagues`.`id`
+	INNER JOIN `countries`
+	ON `leagues`.`country_id` = `countries`.`id`;");
 
 $query->execute();
 
@@ -16,6 +22,12 @@ foreach($shirts as $shirt) {
     echo "<div>";
     echo "<h2>{$shirt['name']}</h2>";
     echo "<p>" . $shirt['season'] . " " . $shirt['type'] . " " . "kit</p>";
+    echo "<div>";
+    echo "<ul>";
+    echo "<li>{$shirt['league_name']}";
+    echo "<li>{$shirt['country']}";
+    echo "<li>{$shirt['brand_name']}";
+    echo "</div>";
     echo "</div>";
 }
 
